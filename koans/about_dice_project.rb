250a4328b959ac3963af804f6a4fe80b15971d95
Logan Hasson <logan.hasson@gmail.com>
2013-10-14 08:34:33 -0400

    pass about_dice_progject.rb

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -6,6 +6,19 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 #   code ...
 # end
 
+class DiceSet
+
+  attr_accessor :values
+
+  def roll(times)
+    self.values = []
+    times.times do
+      self.values << rand(1..6)
+    end
+  end
+
+end
+
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
     dice = DiceSet.new
